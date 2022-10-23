9eef68835b153f39232939db14e3a365dd13086d
Aziz Ur Rehman <azizurrehmanaurofficial@gmail.com>
2022-10-23 07:36:53 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// use for strict mode
+"use strict";
+
+// get id of image preview
+const bulb = document.getElementById("bulb");
+
+function bulbOn() {
+  bulb.src = "Images/bulbOnimg.png";
+}
+function bulbOff() {
+  bulb.src = "Images/bulbOffimg.png";
+}
